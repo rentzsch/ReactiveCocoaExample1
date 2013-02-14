6d6519f5e5f6360e5a5b1582a8f71e6f723ab5e7
rentzsch <jwr.git@redshed.net>
2013-02-14 15:26:41 -0600

    inline uppercase transformer example

--- ReactiveCocoaExample1/AppDelegate.m
+++ ReactiveCocoaExample1/AppDelegate.m
@@ -9,12 +9,31 @@
 #import "AppDelegate.h"
 #import "LowercaseEmitter.h"
 #import "CharacterSignalLogger.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification*)notification {
     RACSignal *lowercaseSignal = [LowercaseEmitter lowercaseEmitter];
     [CharacterSignalLogger loggerWithSignal:lowercaseSignal label:@"lowercase"];
+    
+    // Define transformer inline:
+    RACSignal *uppercaseSignal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [lowercaseSignal subscribeNext:^(NSNumber *charObj) {
+            char c = [charObj charValue];
+            c -= 'a' - 'A';
+            [subscriber sendNext:[NSNumber numberWithChar:c]];
+        }
+        error:^(NSError *error) {
+            [subscriber sendError:error];
+        }
+        completed:^{
+            [subscriber sendCompleted];
+        }];
+        return nil;
+    }];
+    
+    [CharacterSignalLogger loggerWithSignal:uppercaseSignal label:@"inline uppercase"];
 }
 
 @end
